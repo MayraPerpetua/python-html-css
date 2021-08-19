09a769dee9f125d2dd5434dc399bef73f9e70aee
MayraPerpetua <mayrahavhllis@gmail.com>
2021-08-19 23:07:03 +0000

    atualizado

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, request, session, g, redirect, abort, render_template, flash
 
+#configuração 
+DATABASE = "blog.bd"
+SECRET_KEY = 'pudim'
 app = Flask(__name__)
 
 @app.route('/hello')
 def pagina_inicial():
-    return "hello World"
+    return "Hello World"
