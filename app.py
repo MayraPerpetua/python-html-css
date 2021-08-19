97814a1659df03b6b8b688d7fd22814274c30887
MayraPerpetua <mayrahavhllis@gmail.com>
2021-08-19 23:22:39 +0000

    banco de dados

--- app.py
+++ app.py
@@ -3,7 +3,12 @@ from flask import Flask, request, session, g, redirect, abort, render_template, 
 #configuração 
 DATABASE = "blog.bd"
 SECRET_KEY = 'pudim'
+
+def conectar_bd():
+    return sqlite3.connect(app.config['DATABASE'])
+
 app = Flask(__name__)
+app.config.from_object(__name__)
 
 @app.route('/hello')
 def pagina_inicial():
